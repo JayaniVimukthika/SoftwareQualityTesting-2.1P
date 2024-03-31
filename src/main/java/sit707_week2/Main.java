30d67fa3b650b4ef3fdb549161a5e8224d9be8b5
JayaniVimukthika <144791020+JayaniVimukthika@users.noreply.github.com>
2024-03-31 18:47:46 +1100

    alternativeRegistration commit

--- src/main/java/sit707_week2/Main.java
+++ src/main/java/sit707_week2/Main.java
@@ -5,6 +5,7 @@ public class Main
 {
     public static void main( String[] args )
     {
-        SeleniumOperations.officeworks_registration_page("https://www.officeworks.com.au/app/identity/create-account");
+        //SeleniumOperations.officeworks_registration_page("https://www.officeworks.com.au/app/identity/create-account");
+        SeleniumOperations.officeworks_registration_page("https://auth.colesgroupprofile.com.au/login?state=hKFo2SBOaF81ZHN4dGVLQnRrRUxBamxQa2hlOF85OVhiazVGS6FupWxvZ2luo3RpZNkgS2piQUZVYWlRMHlkZEt5elZkR2xJbW5zS1R1LXgwc2ijY2lk2SBPUXNOSVVJUEJvWVJYVUtkUUpCMDZzWXFyMlhMNUJVVQ&client=OQsNIUIPBoYRXUKdQJB06sYqr2XL5BUU&protocol=oauth2&redirect_uri=https%3A%2F%2Fwww.coles.com.au%2Fauth%2Fcallback&audience=customer-services&scope=openid%20read%3Aprofile%20read%3Apreferences%20update%3Apreferences%20update%3Aproduct-list%20read%3Aproduct-list%20update%3Aloyalty-account%20read%3Aloyalty-account%20read%3Acol%20update%3Acol%20sso%3Acol%20offline_access%20read%3Aaddress%20update%3Aaddress%20update%3Aprofile%20read%3Acontact%20update%3Acontact%20delete%3Aaddress&response_type=code&code_challenge_method=S256&code_challenge=qZDUPoqKi9K6q7H1iZ8Mr41mYFpPwZ-MHiB-AnCTtVE&elp%2Faccount-and-payments%2Fhelp-to-login-or-sign-up-to-coles%3Fadobe_mc=MCMID%3D02057110427933197082503521090310633038%7CMCORGID%3D0B3D037254C7DE490A4C98A6%2540AdobeOrg%7CTS%3D1711772675");
     }
 }
